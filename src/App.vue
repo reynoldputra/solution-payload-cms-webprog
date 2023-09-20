db924480564ff9a339e275db360a8279615d9484
rifkiam <rifki.ahmad2003@gmail.com>
2023-09-20 18:31:34 +0700

    feat: add navbar, about

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <header>
-    <div>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <!-- <RouterLink to="/about">About</RouterLink> -->
-      </nav>
+    <div class="">
+      <Navbar />
     </div>
   </header>
   <RouterView />
 </template>
+
+<style>
+.router-link-active {
+  @apply bg-gray-900 text-white hover:text-white hover:bg-gray-900
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="m-5 flex flex-col items-center justify-center gap-5">
+      <div
+        class="block p-6 lg:w-1/2 w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" class="px-6 py-3">Asisten</th>
+                <th scope="col" class="px-6 py-3">NRP</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="(item, index) in asist" :key="index" ref="asist">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  {{ item.name }}
+                </th>
+                <td class="px-6 py-4">{{ item.nrp }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    // template refs
+    mounted() {
+      console.log(this.$refs.asist);
+    },
+    data() {
+      return {
+        asist: [
+          {
+            name: "Midyanisa Yuniar",
+            nrp: 5027211025
+          },
+          {
+            name: "Reynold Putra Merdeka",
+            nrp: 5027211034
+          },
+          {
+            name: "Rifqi Akhmad Maulana",
+            nrp: 5027211035
+          },
+          {
+            name: "Wisnuyasha Faizal",
+            nrp: 5027211036
+          },
+          {
+            name: "Ahnaf Musyaffa",
+            nrp: 5027211038
+          },
+          {
+            name: "Moh. Sulthan Arief Rahmatullah",
+            nrp: 5027211045
+          },
+        ]
+      }
+    }
+  };
+  </script>
+  
